9476d929dc463034cd75d6e328d48902bb347649
alvaroke32 <alvaro.gallegos@estudiante.uam.es>
2026-03-14 15:45:51 +0100

    Create Usuario.java

---
+++ P3/src/Usuario.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Usuario{
+    private String nombre;
+    private int capacidadAmplificacion;
+    private List<Enlace> enlaces;
+
+
+
+
+
+    
+}
