b4f0ad05407c56a59b6393d942483b779653db33
alvaroke32 <alvaro.gallegos@estudiante.uam.es>
2026-03-15 17:18:55 +0100

    Update EjemploDeUsoMensaje.java

--- P3/src/EjemploDeUsoMensaje.java
+++ P3/src/EjemploDeUsoMensaje.java
@@ -3,15 +3,24 @@ public class EjemploDeUsoMensaje{
         Usuario ana = new Usuario("ana", 1);
         Usuario luis = new Usuario("luis", 5);
         Usuario carmen = new Usuario("carmen");
+        Usuario sebas = new Usuario("sebas");
+        Usuario manolo = new Usuario("manolo");
         Mensaje m = new Mensaje("Hi!", 50, ana);
+
         ana.addEnlace(new Enlace(ana, luis, 68));
         ana.addEnlace(carmen, 33);
         System.out.println(m);
-        m.difunde(luis,carmen);
+
+        carmen.addEnlace(manolo, 1);
+        m.difunde(luis,carmen,manolo);
         System.out.println(m);
-        carmen.addEnlace(new Enlace(carmen, luis,11));
-        m.difunde(carmen.getEnlace(luis));
+
+        m.difunde(manolo);
         System.out.println(m);
+
+        sebas.addEnlace(new Enlace(sebas, luis,11));
+        m.difunde(sebas.getEnlace(luis));
+        System.out.println(m); 
     }
 
 
